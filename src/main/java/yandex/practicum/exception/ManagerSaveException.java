68ae4250e92b5cc2be7b3b2333ace41a65534fac
Alena Pletenkina <alenapletenkina@yandex.ru>
2024-06-12 21:28:41 +0300

    ТЗ 7. Поправила чекстаил

--- src/main/java/yandex/practicum/exception/ManagerSaveException.java
+++ src/main/java/yandex/practicum/exception/ManagerSaveException.java
@@ -4,7 +4,6 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message) {
         super(message);
     }
-
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
